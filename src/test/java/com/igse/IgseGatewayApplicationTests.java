e8a2a6594cbdd6fbd7a8ddf7112d2d640d0c8f38
Talha Ansari <talhaansari61@gmail.com>
2023-11-15 20:20:44 +0530

    first

---
+++ src/test/java/com/igse/IgseGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgseGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
