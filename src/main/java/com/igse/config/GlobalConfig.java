b26a731771c4fc08148a4864a0b92a3b6942c8a1
talhaansari04 <talhaansari61@gmail.com>
2024-07-18 20:31:11 +0530

    Added JWT

--- src/main/java/com/igse/config/GlobalConfig.java
+++ src/main/java/com/igse/config/GlobalConfig.java
@@ -1,4 +1,18 @@
-package com.igse.config;
-
-public class GlobalConfig {
-}
+//package com.igse.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.reactive.config.CorsRegistry;
+//import org.springframework.web.reactive.config.WebFluxConfigurer;
+//
+//@Configuration
+//public class GlobalConfig implements WebFluxConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:4200")
+//                .allowedOrigins("Access-Control-Allow-Origin", "http://localhost:4200")
+//                .allowedHeaders("*")
+//                .allowedMethods("*");
+//    }
+//
+//}
---
+++ src/main/java/com/igse/service/JwtService.java
@@ -0,0 +1,32 @@
+package com.igse.service;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+
+@Service
+public class JwtService {
+    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+
+    public String generateToken(String userName,String userRole){
+        return createToken(Map.of("ROLE",userRole),userName);
+    }
+    private String createToken(Map<String, Object> claims, String userName) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userName)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
+                .signWith(getSignkey(), SignatureAlgorithm.HS256).compact();
+    }
+
+    private Key getSignkey() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(SECRET));
+    }
+}
